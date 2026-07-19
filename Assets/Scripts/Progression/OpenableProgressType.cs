dfa5d5657bbce36d8587e3e1604af260fa35bfca
Ludboii <76396356+Ludboii@users.noreply.github.com>
2026-07-20 00:09:24 +0200

    Add openable progress requirement types

---
+++ Assets/Scripts/Progression/OpenableProgressType.cs
@@ -0,0 +1,9 @@
+public enum OpenableProgressType
+{
+    TotalContainers = 0,
+    WeaponCases = 1,
+    CollectionPackages = 2,
+    SouvenirPackages = 3,
+    StickerCapsules = 4,
+    SpecificContainer = 5
+}
